8882072ba938567b6281e370661bb00b30992098
Bryce Cronin <BryceACronin@gmail.com>
2023-02-19 15:44:46 +1100

    Improved file selection & added checks

--- App/main.py
+++ App/main.py
@@ -1,9 +1,12 @@
 import os
 import PySimpleGUI as sg
-import button
 import subprocess
 
+# Theming
+import button
 sg.theme('Material2') 
+button_secondary_background = "#CFD8DC"
+button_secondary_color = "#212121"
 
 # Window layout
 layout_initial = [
@@ -15,11 +18,11 @@ layout_initial = [
     [sg.Image('Images\selectFolder.png', key="image_selectFolder"), sg.Text('Choose Output Folder:', key="text_output"), button.Rounded('Browse', 0.3, key="browse_output")],
     [sg.Text('')],
     [sg.Text('')],
-    [button.Rounded('Continue', 0.3, key="button_configure")],
+    [button.Rounded('Continue', 0.3, key="button_configure", visible=False)],
 ]
 
 layout_configure = [
-    [button.Rounded('Back', 0.3, key="button_back")],
+    [button.Rounded('Back', 0.3, key="button_back", button_color=(button_secondary_color,button_secondary_background),mouseover_colors=(button_secondary_color,"white"))],
     [sg.Text("\nPut device configuration here...\n")],
     [button.Rounded('Generate 3D-Printable File', 0.3, key="button_export")],
 ]
@@ -49,26 +52,43 @@ while True:
 
     # Input A3D File
     if event == "browse_input":
-        file_input = sg.popup_get_file(
+        selected = sg.popup_get_file(
             "Select A3D File",
             file_types=((('A3D Files Only', '*.A3D'),)),
-            no_window = True, )
-        print('new browse input: ' + file_input)
-        window['text_input'].update("Selected A3D File: " + os.path.basename(file_input))
-        window['browse_input'].update("Change")
-        window['image_inputFile'].update('Images\inputFile_done.png')
-
+            no_window = True,
+            history=True )
+        if selected != "":
+            file_input = selected
+            print('new browse input: ' + file_input)
+            window['text_input'].update("Selected A3D File: " + os.path.basename(file_input))
+            window['browse_input'].update("Change")
+            window['image_inputFile'].update('Images\inputFile_done.png')
+        
     # Select Output Location
     if event == "browse_output":
-        file_output = sg.popup_get_folder(
+        selected = sg.popup_get_folder(
             "Choose Output Location",
             no_window = True, )
-        print('new browse output: ' + file_output)  
-        window['text_output'].update("Selected Output Folder: ..." + file_output[-20:])
-        window['browse_output'].update("Change")
-        window['image_selectFolder'].update('Images\selectFolder_done.png')
+        if selected != "":
+            file_output = selected
+            print('new browse output: ' + file_output)  
+            window['text_output'].update("Selected Output Folder: ..." + file_output[-20:])
+            window['browse_output'].update("Change")
+            window['image_selectFolder'].update('Images\selectFolder_done.png')
+
+    # Reset input/output selections
+    if file_input == "":
+        window['text_input'].update("Select A3D File:")
+        window['browse_input'].update("Browse")
+        window['image_inputFile'].update('Images\inputFile.png')
+    if file_output == "":
+        window['text_output'].update("Choose Output Folder:")
+        window['browse_output'].update("Browse")
+        window['image_selectFolder'].update('Images\selectFolder.png')
 
     # Continue to configure
+    if file_input != "" and file_output != "":
+        window['button_configure'].update(visible=True)
     if event == 'button_configure':
         window['column_initial'].update(visible=False)
         window['column_configure'].update(visible=True)
@@ -85,15 +105,5 @@ while True:
             # Make sure A3D file and output folder has been selected
         print(('openscad -o ' + file_output + os.path.basename(file_input) + ' -D"vartest2=5" ' + file_input))
         subprocess.Popen('openscad -o ' + file_output + '/' + os.path.basename(file_input)[:-4] + '_output.stl -D"vartest2=5" ' + file_input)
-
-    # Reset input/output selections
-    if file_input == "":
-        window['text_input'].update("Select A3D File:")
-        window['browse_input'].update("Browse")
-        window['image_inputFile'].update('Images\inputFile.png')
-    if file_output == "":
-        window['text_output'].update("Choose Output Folder:")
-        window['browse_output'].update("Browse")
-        window['image_selectFolder'].update('Images\selectFolder.png')
 
 window.close()
